2563faf8614234699299edc5fdc203895b227de1
Liuda Hraivoronska <liuda.hraivoronska@ab-soft.net>
2022-10-21 10:33:16 +0300

    Add FolkDanceRobot

---
+++ src/com/company/FolkDanceRobot.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class FolkDanceRobot extends DancingRobot {
+    @Override
+    public String getDanceType1() {
+        return "Ой  у лузі";
+    }
+
+    @Override
+    public String getDanceType2() {
+        return "Folk dance 2";
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	DancingRobot robot1 = new BalletDancingRobot();
+    DancingRobot robot2 = new FolkDanceRobot();
+        printRobotDanceTypes(robot1);
+        printRobotDanceTypes(robot2);
+    }
+
+    private static void printRobotDanceTypes(DancingRobot robot1) {
         System.out.println(robot1.getDanceType1());
         System.out.println(robot1.getDanceType2());
         System.out.println(robot1.getDefaultDanceType());
-
+        System.out.println("====================");
     }
 }
